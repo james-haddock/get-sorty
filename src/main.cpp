6c117afadda0706f86157d66525bb0af0aaadc38
James Haddock <123553781+james-haddock@users.noreply.github.com>
2024-01-16 10:36:31 +0000

    installed gtest and created directory_monitor.cpp file and header

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <filesystem>
+#include <set>
+#include <thread>
+#include <chrono>
+#include <vector>
+
+namespace fs = std::filesystem;
+
+int main() {
+    std::vector<std::string> file_paths;
+    std::string path;
+    std::cout << "Please type your file path:";
+
+    return 0;
+}
