d3a2d6611938f86b19422020ad3f1a55ee21731a
James Haddock <123553781+james-haddock@users.noreply.github.com>
2024-01-16 22:13:32 +0000

    created thread for directory observer

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,25 @@
 #include <thread>
 #include <chrono>
 #include <vector>
+#include "infrastructure/directory_monitor.h"
 
-namespace fs = std::filesystem;
+// namespace fs = std::filesystem;
 
 int main() {
-    std::vector<std::string> file_paths;
+    std::vector<std::filesystem::path> file_paths;
     std::string path;
     std::cout << "Please type your file path:";
+    std::cin >> path;
+    file_paths.push_back(path);
+    bool stop_observing = false;
+    DirectoryMonitor directoryMonitor(file_paths);
+    directoryMonitor.start_monitoring(stop_observing);
+
+    std::string command;
+    while (command != "stop") {
+        std::cout << "Type 'stop' to stop monitoring and exit the program: ";
+        std::cin >> command;
+    }
 
     return 0;
-}
+};
