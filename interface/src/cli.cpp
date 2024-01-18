d51f7d4757cf9708d1e7308fca829130edae1451
James Haddock <123553781+james-haddock@users.noreply.github.com>
2024-01-18 22:52:26 +0000

    implmented command pattern to initialise task automation

--- interface/src/cli.cpp
+++ interface/src/cli.cpp
@@ -2,20 +2,20 @@
 
 int main(int argc, char** argv) {
 
-  int menu_selection;
-  do {
-    std::cout << "Welcome to Get Sorty!\n";
-    std::cout << "Main Menu\n";
-    std::cout
-        << "Please enter your numerical selection below and press enter:\n";
-    std::cout << "1. Start Observing Directories.\n";
-    std::cout << "2. Edit Directories.\n\n";
-    std::cout << "3. Quit.\n\n";
-    std::cout << "Input option:\n";
-    std::cin >> menu_selection;
-    switch (menu_selection) {
-    case 1:
-    }
+//   int menu_selection;
+//   do {
+//     std::cout << "Welcome to Get Sorty!\n";
+//     std::cout << "Main Menu\n";
+//     std::cout
+//         << "Please enter your numerical selection below and press enter:\n";
+//     std::cout << "1. Start Observing Directories.\n";
+//     std::cout << "2. Edit Directories.\n\n";
+//     std::cout << "3. Quit.\n\n";
+//     std::cout << "Input option:\n";
+//     std::cin >> menu_selection;
+//     switch (menu_selection) {
+//     case 1:
+//     }
 
     return 0;
 }
